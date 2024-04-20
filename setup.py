e93e67c5c29644a31ae4203bca1a32a5e6401783
ckf10000 <ckf10000@sina.com>
2024-04-20 12:29:08 +0800

    first

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-apollo-proxy',
-    version='0.1.0',
+    version='0.1.1',
     description='This is my apollo proxy client package',
     long_description='This is my apollo proxy client package',
     author='ckf10000',
