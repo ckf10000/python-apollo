dd181c1604e634a1906b33d3a56c58330a0986de
ckf10000 <ckf10000@sina.com>
2024-07-22 17:03:16 +0800

    firts

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-apollo-proxy',
-    version='0.1.6',
+    version='0.1.7',
     description='This is my apollo proxy client package',
     long_description='This is my apollo proxy client package',
     author='ckf10000',
